c66bb14ee4c8638d3ff3bc1becf2e37da289598a
DSJung <DSJung@realtimevisual.com>
2017-10-17 13:41:47 +0900

    change class

--- DICOMImage.py
+++ DICOMImage.py
@@ -4,27 +4,48 @@ import numpy as np
 def getBestImage(images):
     return images[40]
 
-def readDICOMImage(filename):
+
+class DicomData():
+    __count=0
+    Dicom_array=[]
     reader=sitk.ImageFileReader()
-    reader.SetFileName(filename)
-    dicomImage=reader.Execute()
+    def __init__(self, filename):
+        DicomData.reader.SetFileName(filename)
+        dicomImage=DicomData.reader.Execute()
+        if dicomImage.HasMetaDataKey('0008|0070') is not True:
+            print('not DICOM image, '+type(dicomImage))
+        elif dicomImage.GetMetaData('0008|0070')!='Siemens ':
+            print('not Siemens, but '+dicomImage.GetMetaData('0008|0070'))
+        else:
+            DicomData.Dicom_array.append(self)
+            self.order=DicomData.__count
+            DicomData.__count+=1
+            # print('return Transform Matrix')
+            self.SID=float(dicomImage.GetMetaData('0018|1110'))
+            self.SOD=float(dicomImage.GetMetaData('0018|1111'))
+            # self.mag=float(dicomImage.GetMetaData('0018|1114'))
+            self.angle1=float(dicomImage.GetMetaData('0018|1510'))
+            self.angle1 = np.deg2rad(self.angle1)
+            self.angle2=float(dicomImage.GetMetaData('0018|1511'))
+            self.angle2 = np.deg2rad(self.angle2)
+            self.pixel_spacing=np.array(dicomImage.GetMetaData('0018|1164').split('\\')).astype(float)
+            self.images = sitk.GetArrayFromImage(dicomImage)
+            self.image = getBestImage(self.images)
+    
+    def info(self):
+        print('Dicom file '+str(self.order)+' : angles ('+str(self.angle1)+', '+str(self.angle2)+' has '+len(self.images)+' images')
 
-    if dicomImage.HasMetaDataKey('0008|0070') is not True:
-        print('not DICOM image, '+type(dicomImage))
-    elif dicomImage.GetMetaData('0008|0070')!='Siemens ':
-        print('not Siemens, but '+dicomImage.GetMetaData('0008|0070'))
-    else:
-        # print('return Transform Matrix')
-        SID=float(dicomImage.GetMetaData('0018|1110'))
-        SOD=float(dicomImage.GetMetaData('0018|1111'))
-        # mag=float(dicomImage.GetMetaData('0018|1114'))
-        angle1=float(dicomImage.GetMetaData('0018|1510'))
-        angle1 = np.deg2rad(angle1)
-        angle2=float(dicomImage.GetMetaData('0018|1511'))
-        angle2 = np.deg2rad(angle2)
-        pixel_spacing=np.array(dicomImage.GetMetaData('0018|1164').split('\\')).astype(float)
+    def compute_initial_transform_matrix(self):
+        self.skew=0
+        self.tls=np.array([[0.0],[0.0],[0.0],[1.0]])
 
-    images = sitk.GetArrayFromImage(dicomImage)
-    image = getBestImage(images)
+        self.roty=np.array([[np.cos(self.angle1),0.0,-np.sin(self.angle1)],[0.0,1.0,0.0],[np.sin(self.angle1),0.0,np.cos(self.angle1)]])
+        self.rotx=np.array([[1.0,0.0,0.0],[0.0,np.cos(self.angle2),-np.sin(self.angle2)],[0.0,np.sin(self.angle2),np.cos(self.angle2)]])
 
-    return image, {'SID':SID, 'SOD':SOD, 'PA':angle1, 'SA':angle2, 'PS':pixel_spacing}
+        self.rot=np.matmul(self.rotx,self.roty)
+        self.rot=np.vstack((self.rot,[0.0,0.0,0.0]))
+
+        self.K=np.array([[self.SID/(self.pixel_spacing[0]),self.SID/(self.pixel_spacing[1])*self.skew,256,self.SOD*256],[0,self.SID/(self.pixel_spacing[1]),256,self.SOD*256],[0,0,1,self.SOD]])
+
+        self.P=np.matmul(self.K,np.concatenate((self.rot,self.tls),axis=1))
+        
--- ProjectionMatrix.py
+++ ProjectionMatrix.py
@@ -2,61 +2,29 @@ import SimpleITK as sitk
 import numpy as np
 from tkinter.filedialog import askopenfilenames
 import cv2
-from DICOMImage import readDICOMImage
-
-def Initial_transform_Matrix(Data):
-    DEGTORAD = np.pi / 180.0
-    img, metadata = readDICOMImage(Data)
-    skew=0
-    
-    SID = metadata['SID']
-    SOD = metadata['SOD']
-    angle1 = metadata['PA']
-    angle2 = metadata['SA']
-    pixel_spacing = metadata['PS']
-
-    roty=np.array([[np.cos(angle1),0.0,np.sin(angle1)],[0.0,1,0.0],[-np.sin(angle1),0.0,-np.cos(angle1)]])
-    rotx=np.array([[1.0,0.0,0.0],[0.0,np.cos(angle2),-np.sin(angle2)],[0.0,np.sin(angle2),np.cos(angle2)]])
-
-    rot=np.matmul(rotx,roty)
-
-    rot=np.vstack((rot,[0.0,0.0,0.0]))
-    # print(rot)
-
-    # K=np.array([[mag/(pixel_spacing[0]),mag/(pixel_spacing[1])*skew,0],[0,mag/(pixel_spacing[1]),0],[0,0,1]])
-    K=np.array([[SID/(pixel_spacing[0]),SID/(pixel_spacing[1])*skew,256,SOD*256],[0,SID/(pixel_spacing[1]),256,SOD*256],[0,0,1,SOD]])
-    # tls=np.zeros([3,1])
-    tls=np.array([[0.0],[0.0],[0.0],[1.0]])
-    print('angle is '+str(angle1/DEGTORAD)+','+str(angle2/DEGTORAD))
-
-    P=np.matmul(K,np.concatenate((rot,tls),axis=1))
-    return P, K, rot, tls
-
 
 def global_line_from_image_point(point,Data):
-    [P, K, rot, tls]=Initial_transform_Matrix(Data)
     [u,v]=point
-    coef1=np.array([[u/K[0][0], v/K[1][1], 1.0]])
-    coef2=K[2][3]*np.array([[u/K[0][0], v/K[1][1], 0.0]])
-#     print(coef2)
-    coef1_global=np.matmul(np.linalg.inv(rot[0:3][:]),coef1.T)
-    coef2_global=np.matmul(np.linalg.inv(rot[0:3][:]),(coef2.T-tls[0:3]))
-#     print((coef2.T-tls[0:3]))
+    coef1=np.array([[u/Data.K[0][0], v/Data.K[1][1], 1.0]])
+    coef2=Data.K[2][3]*np.array([[u/Data.K[0][0], v/Data.K[1][1], 0.0]])
+    # print(coef2)
+    coef1_global=np.matmul(np.linalg.inv(Data.rot[0:3][:]),coef1.T)
+    coef2_global=np.matmul(np.linalg.inv(Data.rot[0:3][:]),(coef2.T-Data.tls[0:3]))
+    # print((coef2.T-Data.tls[0:3]))
     
     return coef1_global, coef2_global
     
 
 def projected_epipolar_line(point,Data_from,Data_to):
     [coef1_global, coef2_global]=global_line_from_image_point(point,Data_from)
-    [P, K, rot, tls]=Initial_transform_Matrix(Data_to)
-    coef1=np.matmul(rot[0:3][:],coef1_global)
-    coef2=np.matmul(rot[0:3][:],coef2_global)+tls[0:3]
+    coef1=np.matmul(Data_to.rot[0:3][:],coef1_global)
+    coef2=np.matmul(Data_to.rot[0:3][:],coef2_global)+Data_to.tls[0:3]
     coef1=np.vstack([coef1,1])
     coef2=np.vstack([coef2,1])
-    print(coef1_global)
-    pixel1=np.matmul(P,coef2)
+    # print(coef1_global)
+    pixel1=np.matmul(Data_to.P,coef2)
     pixel1/=pixel1[2]
-    pixel2=np.matmul(P,coef1+coef2)
+    pixel2=np.matmul(Data_to.P,coef1+coef2)
     pixel2/=pixel2[2]
     coef_img=np.array([pixel2[1]-pixel1[1], pixel1[0]-pixel2[0], pixel2[0]*pixel1[1]-pixel1[0]*pixel2[1]])
     
