522719a4aee227d89e81736286076b2677aa115f
DSJung <DSJung@realtimevisual.com>
2017-10-20 15:20:31 +0900

    epipolar geometry

--- DICOMImage.py
+++ DICOMImage.py
@@ -11,24 +11,24 @@ class DicomData():
     reader=sitk.ImageFileReader()
     def __init__(self, filename):
         DicomData.reader.SetFileName(filename)
-        dicomImage=DicomData.reader.Execute()
-        if dicomImage.HasMetaDataKey('0008|0070') is not True:
-            print('not DICOM image, '+type(dicomImage))
-        elif dicomImage.GetMetaData('0008|0070')!='Siemens ':
-            print('not Siemens, but '+dicomImage.GetMetaData('0008|0070'))
+        self.dicomImage=DicomData.reader.Execute()
+        if self.dicomImage.HasMetaDataKey('0008|0070') is not True:
+            print('not DICOM image, '+type(self.dicomImage))
+        elif self.dicomImage.GetMetaData('0008|0070')!='Siemens ':
+            print('not Siemens, but '+self.dicomImage.GetMetaData('0008|0070'))
         else:
             DicomData.Dicom_array.append(self)
             self.order=DicomData.__count
             DicomData.__count+=1
-            self.SID=float(dicomImage.GetMetaData('0018|1110'))
-            self.SOD=float(dicomImage.GetMetaData('0018|1111'))
+            self.SID=float(self.dicomImage.GetMetaData('0018|1110'))
+            self.SOD=float(self.dicomImage.GetMetaData('0018|1111'))
             # self.mag=float(dicomImage.GetMetaData('0018|1114'))
-            self.PA=float(dicomImage.GetMetaData('0018|1510'))
+            self.PA=float(self.dicomImage.GetMetaData('0018|1510'))
             self.PA = np.deg2rad(self.PA)
-            self.SA=float(dicomImage.GetMetaData('0018|1511'))
+            self.SA=float(self.dicomImage.GetMetaData('0018|1511'))
             self.SA = np.deg2rad(self.SA)
-            self.PS=np.array(dicomImage.GetMetaData('0018|1164').split('\\')).astype(float)
-            self.images = sitk.GetArrayFromImage(dicomImage)
+            self.PS=np.array(self.dicomImage.GetMetaData('0018|1164').split('\\')).astype(float)
+            self.images = sitk.GetArrayFromImage(self.dicomImage)
             self.img = getBestImage(self.images)
             self.tls=np.array([[0.0],[0.0],[0.0],[1.0]])
             self.skew=0
@@ -38,16 +38,16 @@ class DicomData():
         print('Dicom file '+str(self.order)+' : angles ('+str(self.PA)+', '+str(self.SA)+' has '+len(self.images)+' images')
 
     def compute_initial_transform_matrix(self):
+        # self.roty=np.array([[np.cos(self.PA),0.0,-np.sin(self.PA)],[0.0,1.0,0.0],[np.sin(self.PA),0.0,np.cos(self.PA)]])
+        # self.rotx=np.array([[1.0,0.0,0.0],[0.0,np.cos(self.SA),-np.sin(self.SA)],[0.0,np.sin(self.SA),np.cos(self.SA)]])
         self.roty=np.array([[np.cos(self.PA),0.0,-np.sin(self.PA)],[0.0,1.0,0.0],[np.sin(self.PA),0.0,np.cos(self.PA)]])
-        self.rotx=np.array([[1.0,0.0,0.0],[0.0,np.cos(self.SA),-np.sin(self.SA)],[0.0,np.sin(self.SA),np.cos(self.SA)]])
-        
-        # if dicomImage.GetMetaData('0020|0013')=='2 ':
-            
-        # else:
-        #     self.roty=np.array([[np.cos(self.PA),0.0,-np.sin(self.PA)],[0.0,1.0,0.0],[np.sin(self.PA),0.0,np.cos(self.PA)]])
-        #     self.rotx=np.array([[1.0,0.0,0.0],[0.0,np.cos(self.SA),-np.sin(self.SA)],[0.0,np.sin(self.SA),np.cos(self.SA)]])
-        
-        self.rot=np.matmul(self.rotx,self.roty)
+        if self.dicomImage.GetMetaData('0020|0013')=='2 ':
+            self.rotz=np.array([[np.cos(self.SA),np.sin(self.SA),0.0],[-np.sin(self.SA),np.cos(self.SA),0.0],[0.0,0.0,1.0]])
+            self.rot=np.matmul(self.rotz,self.roty)
+        else:
+            self.rotx=np.array([[1.0,0.0,0.0],[0.0,np.cos(self.SA),-np.sin(self.SA)],[0.0,np.sin(self.SA),np.cos(self.SA)]])
+            self.rot=np.matmul(self.rotx,self.roty)
+                
         self.rot=np.vstack((self.rot,[0.0,0.0,0.0]))
 
         self.K=np.array([[self.SID/(self.PS[0]),self.SID/(self.PS[1])*self.skew,256,self.SOD*256],[0,self.SID/(self.PS[1]),256,self.SOD*256],[0,0,1,self.SOD]])
